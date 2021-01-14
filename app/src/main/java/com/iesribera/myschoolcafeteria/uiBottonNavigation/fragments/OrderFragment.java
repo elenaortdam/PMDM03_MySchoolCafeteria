fa7ff9885ec5ac9246d2c2a77644f9da975f77b7
HelenaOrt <elenaortdam@gmail.com>
2021-01-14 19:10:33 +0100

    Añadido que cuando se cree un envío te mande al maps

--- app/src/main/java/com/iesribera/myschoolcafeteria/uiBottonNavigation/fragments/OrderFragment.java
+++ app/src/main/java/com/iesribera/myschoolcafeteria/uiBottonNavigation/fragments/OrderFragment.java
@@ -12,8 +12,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -33,7 +32,6 @@ import com.iesribera.myschoolcafeteria.models.Product;
 import com.iesribera.myschoolcafeteria.models.User;
 import com.iesribera.myschoolcafeteria.models.UserEvent;
 import com.iesribera.myschoolcafeteria.uiBottonNavigation.adapters.ProductAdapter;
-import com.iesribera.myschoolcafeteria.uiBottonNavigation.map.MapsFragment;
 
 import java.io.File;
 import java.io.IOException;
@@ -65,7 +63,11 @@ public class OrderFragment extends Fragment implements View.OnClickListener {
 		recyclerView = view.findViewById(R.id.productRecyclerView);
 		TextView userName = view.findViewById(R.id.userName);
 		orderButton = view.findViewById(R.id.orderButton);
-		orderButton.setOnClickListener(v -> createOrder());
+		orderButton.setOnClickListener(v -> {
+			createOrder();
+			showMapActivity(v);
+
+		});
 		userName.setText(User.getInstance().getName());
 		DatabaseReference mDatabase = FirebaseDatabase.getInstance()
 													  .getReference("/products");
@@ -93,25 +95,6 @@ public class OrderFragment extends Fragment implements View.OnClickListener {
 			}
 
 		});
-		if (view.findViewById(R.id.fragment_container) != null) {
-
-			// However, if we're being restored from a previous state,
-			// then we don't need to do anything and should return or else
-			// we could end up with overlapping fragments.
-			if (savedInstanceState != null) {
-			}
-
-			// Create an instance of Fragment1
-			MapsFragment firstFragment = new MapsFragment();
-
-			// In case this activity was started with special instructions from an Intent,
-			// pass the Intent's extras to the fragment as arguments
-			firstFragment.setArguments(getActivity().getIntent().getExtras());
-
-			// Add the fragment to the 'fragment_container' FrameLayout
-			getChildFragmentManager().beginTransaction()
-									 .add(R.id.fragment_container, firstFragment).commit();
-		}
 		return view;
 	}
 
@@ -142,7 +125,6 @@ public class OrderFragment extends Fragment implements View.OnClickListener {
 				FirebaseDatabase.getInstance().getReference("/user-orders");
 		orderReference.push().setValue(order);
 		createOrderCreatedNotification(order);
-		showMapActivity();
 /*
 		orderReference.runTransaction(new Transaction.Handler() {
 			@NonNull
@@ -185,7 +167,11 @@ public class OrderFragment extends Fragment implements View.OnClickListener {
 		createOrder.addToDatabase();
 	}
 
-	private void showMapActivity() {
+	private void showMapActivity(View view) {
+//		NavDirections action =
+//				SpecifyAmountFragmentDirections
+//						.actionSpecifyAmountFragmentToConfirmationFragment();
+		Navigation.findNavController(view).navigate(R.id.action_order_to_map);
 		/*
 		Intent i = new Intent(getContext(), MapActivity.class);
 		Toast.makeText(getActivity(),
@@ -193,13 +179,15 @@ public class OrderFragment extends Fragment implements View.OnClickListener {
 		startActivity(i);
 
 		 */
-
+/*
 		Fragment fragment = new MapsFragment();
 
 		FragmentManager fm = getFragmentManager();
 		FragmentTransaction transaction = fm.beginTransaction();
 		transaction.replace(R.id.map, fragment);
 		transaction.commit();
+
+ */
 
 //		FragmentManager fragmentManager=getActivity().getFragmentManager();
 //		FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
